09949dc8dcede1577490b32573878627fe359218
Chandrakant Walke <chandrakant.walke@neo-world.com>
2022-12-14 12:29:22 +0530

    update

--- app/src/main/java/com/example/simplelibrary/MainActivity.kt
+++ app/src/main/java/com/example/simplelibrary/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.simplelibrary
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.toastmessage.DisplayToast
+import com.example.toastmessage.DisplayToastOne
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +11,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         DisplayToast.simpleToast(this,"welcome")
+        DisplayToastOne.simpleToast(this,"Welcome")
     }
 }
